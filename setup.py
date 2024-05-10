92bdaf995f32102dc76f0505316a24e03f7b0b0d
liumin <minliu2@cs.cmu.edu>
2024-05-10 16:15:17 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="depth_anything",
+    version="1.0.0",
+    install_requires=[
+        "gradio-imageslider",
+        "gradio==4.14.0",
+        "torch",
+        "torchvision",
+        "opencv-python",
+        "huggingface-hub"
+    ],
+    url="none",
+    packages=["depth_anything"],
+)
